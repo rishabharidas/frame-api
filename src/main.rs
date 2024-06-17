e4b9d6932b3c5df02de566f257b8e4f508111d34
rishabharidas <rishabharidas010@gmail.com>
2024-06-17 14:53:21 +0530

    use data from a json file to serve data

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
 use rocket::http::Status;
 use rocket::serde::json::{json, Value};
+use std::fs::File;
+use std::io::prelude::*;
 
 #[macro_use]
 extern crate rocket;
 
-static IMAGES: &[&str] = &[
-    "https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
-    "https://images.unsplash.com/photo-1591775161903-497839a443c1?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3MzR8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
-    "https://images.unsplash.com/photo-1617296539691-67feaadf389e?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3NjF8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
-    "https://images.unsplash.com/photo-1542835435-4fa357baa00b?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3NzN8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
-    "https://images.unsplash.com/photo-1553184570-557b84a3a308?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY2NTF8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
-    "https://images.unsplash.com/photo-1603855873822-0931a843ee3a?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3ODJ8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
-    "https://images.unsplash.com/photo-1509130446053-899ae7358ce6?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY2NjF8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
-    "https://images.unsplash.com/photo-1508931133503-b1944a4ecdd5?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY3OTV8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
-    "https://mui.com/static/images/cards/paella.jpg",
-    "https://mui.com/static/images/cards/contemplative-reptile.jpg",
-    "https://images.unsplash.com/photo-1597077962467-be16edcc6a43?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY2MzZ8&ixlib=rb-4.0.3&w=300&h=300&auto=format&fit=max",
-];
+fn get_file_data() -> Value {
+    let mut file = File::open("static/images.json").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("cannt read file");
+    let data: Value = serde_json::from_str(&contents).expect("error occured");
+    data
+}
 
 #[get("/")]
 fn index() -> String {
@@ -25,20 +21,16 @@ fn index() -> String {
 
 #[get("/")]
 fn all_images() -> Result<Value, Status> {
-    Ok(json!({
-        "images": IMAGES
-    }))
+    let images_data = get_file_data();
+    Ok(serde_json::json!({"images": images_data}))
 }
 
 #[get("/<_id>")]
 fn get_image(_id: usize) -> Result<Value, Status> {
-    if _id >= IMAGES.len() {
-        Ok(json!({
-            "url": IMAGES[_id]
-        }))
-    } else {
-        return Err(Status::NoContent);
-    }
+    let images_data = get_file_data();
+    Ok(json!({
+        "url": images_data[_id]
+    }))
 }
 
 #[launch]
